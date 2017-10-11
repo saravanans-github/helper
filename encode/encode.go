d1b95e9d55806a8d62a9e29d9cdb363ff7f943c0
Saravanan <saravanans.github@outlook.sg>
2017-10-11 18:02:30 +0800

    refactored function names

--- encode/encode.go
+++ encode/encode.go
@@ -6,8 +6,8 @@ import (
 	"log"
 )
 
-// GetHexStringAsBin is a string helper which interprets its argument string as a hex number and returns it as a []byte
-func GetHexStringAsBin(s string) []byte {
+// HexStringToBin is a string helper which interprets its argument string as a hex number and returns it as a []byte
+func HexStringToBin(s string) []byte {
 	h, err := hex.DecodeString(s)
 	if err != nil {
 		log.Fatal(err)
@@ -15,12 +15,12 @@ func GetHexStringAsBin(s string) []byte {
 	return h
 }
 
-// Base64Encode base64 encode helper which returns its argument string as a base64 encoded string
-func Base64Encode(s string) string {
-	return Base64EncodeFromBytes([]byte(s))
+// StringToBase64 base64 encode helper which returns its argument string as a base64 encoded string
+func StringToBase64(s string) string {
+	return BytesToBase64([]byte(s))
 }
 
-// Base64EncodeFromBytes is a base64 encode helper which returns its argument []byte as a base64 encoded string
-func Base64EncodeFromBytes(b []byte) string {
+// BytesToBase64 is a base64 encode helper which returns its argument []byte as a base64 encoded string
+func BytesToBase64(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
 }
--- encode/encode_test.go
+++ encode/encode_test.go
@@ -5,25 +5,25 @@ import (
 	"testing"
 )
 
-func TestGetHexStringAsBin(t *testing.T) {
+func TestHexStringToBin(t *testing.T) {
 	hexConst := "48656c6c6f20476f7068657221" // this is the hex value of "Hello Gopher!"
-	s := hex.EncodeToString(GetHexStringAsBin(hexConst))
+	s := hex.EncodeToString(HexStringToBin(hexConst))
 	if s != hexConst {
 		t.FailNow()
 	}
 }
 
-func TestBase64Encode(t *testing.T) {
+func TestStringToBase64(t *testing.T) {
 	stringConst := "Hello World" // base64 of "Hello World" = SGVsbG8gV29ybGQ=
-	if Base64Encode(stringConst) != "SGVsbG8gV29ybGQ=" {
+	if StringToBase64(stringConst) != "SGVsbG8gV29ybGQ=" {
 		t.FailNow()
 	}
 }
 
 // Base64EncodeFromBytes is a base64 encode helper which returns its argument []byte as a base64 encoded string
-func TestBase64EncodeFromBytes(t *testing.T) {
+func TestBytesToBase64(t *testing.T) {
 	byteConst := []byte("Hello World") // base64 of "Hello World" = SGVsbG8gV29ybGQ=
-	if Base64EncodeFromBytes(byteConst) != "SGVsbG8gV29ybGQ=" {
+	if BytesToBase64(byteConst) != "SGVsbG8gV29ybGQ=" {
 		t.FailNow()
 	}
 }
